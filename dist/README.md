7693a46adf2974b5e99f14819ac18c1373b15933
Rich <r.c.kim3@gmail.com>
2022-03-17 22:26:11 -0400

    finished index.js

---
+++ dist/README.md
@@ -0,0 +1,44 @@
+## c
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+    
+## Installation
+
+Run this programming using 
+
+```bash
+    e
+```
+
+## Usage
+    
+d
+
+## License
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    
+## Contributing
+    
+g
+
+## Tests
+
+Run the below command to test the program.
+```bash
+    f
+```
+    
+## Questions
+
+For any questions, please contact me using the information below:
+
+Github: @a
+    
+Email: b
--- index.js
+++ index.js
@@ -1,111 +1,119 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { listenerCount } = require('process');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
         message: 'What is your Github username?',
         name: 'username',
       },
       {
-        type: 'input',
         message: 'What is your email?',
         name: 'email',
       },
       {
-        type: 'input',
         message: 'What is the project\'s name?',
         name: 'projectName',
       },
       {
-        type: 'input',
         message: 'What is your project about?',
         name: 'description',
       },
       {
-        type: 'input',
+        type: 'list',
         message: 'What license does your project have?',
         name: 'license',
+        choices: [
+            {name: 'MIT'},
+            {name: 'Apache'},
+            {name: 'ISC'},
+            {name: 'GPLv3'}
+          ]
       },
       {
-        type: 'input',
         message: 'What command should be used to install dependencies?',
         name: 'install',
       },
       {
-        type: 'input',
         message: 'What is your test command?',
         name: 'test',
       },
       {
-        type: 'input',
         message: 'What does the user need to know about contributing?',
         name: 'contributing',
       },
 ];
 
 // TODO: Create a function to write README file
+let READMEContent = ({username, email, projectName, description, license, install, test, contributing}) =>
+
+`## ${projectName}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+    
+## Installation
+
+Run this programming using 
+
+\`\`\`bash
+    ${install}
+\`\`\`
+
+## Usage
+    
+${description}
+
+## License
+
+${license}
+    
+## Contributing
+    
+${contributing}
+
+## Tests
+
+Run the below command to test the program.
+\`\`\`bash
+    ${test}
+\`\`\`
+    
+## Questions
+
+For any questions, please contact me using the information below:
+
+Github: @${username}
+    
+Email: ${email}
+`
+
 function writeToFile(fileName, data) {
-
-    const READMEContent = `## ${data['projectName']}
-
-    ## Table of Contents
-    1. [Installation](#installation)
-    2. [Usage](#usage)
-    3. [License](#license)
-    4. [Contributing](#contributing)
-    5. [Tests](#tests)
-    6. [Questions](#questions)
-    
-    ## Installation
-
-    Run this programming using 
-
-    \`\`\`bash
-    ${data['test']}
-    \`\`\`
-
-    ## Usage
-    
-    ${data['description']}
-
-    ## License
-
-    ${data['license']}
-    
-    ## Contributing
-    
-    ${data['contributing']}
-
-    ## Tests
-
-    Run the below command to test the program.
-    \`\`\`bash
-    ${data['test']}
-    \`\`\`
-    
-    ## Questions
-
-    For any questions, please contact me using the information below:
-
-    Github: @${data['username']}
-    
-    Email: ${data['email']}
-    `
-
-    fs.writeFile(`${fileName}.md`, READMEContent, (err) => {
-        err ? console.error(err) : console.log('Success!')
+    fs.writeFile(fileName, data, {flag: 'w'}, (err) => {
+        err ? console.error(err) : console.log('Success!');
     })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt(questions)
-    .then(writeToFile)
+    inquirer.prompt(questions).then(answers => {
+        var badges = {
+            MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+            Apache: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+            ISC: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+            GPLv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+        }
+        answers.license = badges[answers.license]
+        writeToFile('./dist/README.md', READMEContent(answers));
+    }) 
 }
 
 // Function call to initialize app
